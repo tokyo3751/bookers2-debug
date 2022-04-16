998c1961794f389db40c472622a60fe7c0675133
tokyo3751 <tomizawa3751@gmail.com>
2022-04-16 06:58:29 +0000

    [Update]bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root "homes#top"
   get "home/about"=>"homes#about", as: "about"
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
    resources :book_comments, only: [:create,:destroy]
    resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-
+  resources :users, only: [:index,:show,:edit,:update]do
+    resource :relationships, only: [:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
